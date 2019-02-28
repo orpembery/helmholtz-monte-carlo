86ce7b51641800e1aeea67f6a58afdee7d78a8b7
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-02-28 13:00:14 +0000

    Point generation changed to allow for randomised QMC

--- helmholtz_monte_carlo/point_generation.py
+++ helmholtz_monte_carlo/point_generation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import latticeseq_b2
+from warnings import warn
 
 class mc_points(object):
     """Holds a collection of integration points in many dimensions.
@@ -17,7 +18,7 @@ class mc_points(object):
 
     
 
-    def __init__(self,J,M,point_generation_method,seed=1):
+    def __init__(self,J,N,point_generation_method,seed=1):
         """Generates either Monte-Carlo or Quasi-Monte-Carlo integration
         points on the multi-dimensional [-1/2,1/2] cube.
 
@@ -26,36 +27,44 @@ class mc_points(object):
         J - positive int - the length of the KL-like expansion in the
         definition of n.
 
-        M - positive int - 2**M is the number of integration points to use.
+        N - positive int - N (should be of the form 2**M for qmc
+        methods) is the number of integration points to use.
 
-        point_generation_method - either 'qmc' or 'mc'. 'qmc' means a QMC
-        lattice rule is used to generate the points, whereas 'mc' means the
-        points are randomly generated according to a uniform distribution on
-        the unit cube.
+        point_generation_method - either 'qmc' or 'mc'. 'qmc' means a
+        QMC lattice rule is used to generate the points, whereas 'mc'
+        means the points are randomly generated according to a uniform
+        distribution on the unit cube.
 
         seed - seed with which to start the randomness.
 
         Outputs:
 
-        points - 2**M x J numpy array, where each row is the coordinates
+        points - N x J numpy array, where each row is the coordinates
         of an integration point.
 
         """
 
         self._J = J
 
-        self._M = M
+        self._N = N
 
         self._point_generation_method = point_generation_method
 
         if point_generation_method is 'qmc':
-            # Generate QMC points on [-1/2,1/2]^J using Dirk Nuyens' code
+
+            self._M = int(np.log2(self._N))
+
+            if 2**self._M != self._N:
+                warn("N is not a power of 2, instead using N = " + str(2**self._M))
+                
+            # Generate QMC points on [-1/2,1/2]^J using Dirk Nuyens'
+            # code
             qmc_generator = latticeseq_b2.latticeseq_b2(s=J)
 
             qmc_points = []
 
             # The following range will have M as its last term
-            for m in range((M+1)):
+            for m in range((self._M+1)):
                 qmc_points.append(qmc_generator.calc_block(m))
 
             self.points = qmc_points[0]
@@ -64,7 +73,7 @@ class mc_points(object):
                 self.points = np.vstack((self.points,qmc_points[ii]))
 
         elif point_generation_method is 'mc':
-            self.points = np.random.rand(2**M,J)
+            self.points = np.random.rand(self._N,J)
 
         self.points -= 0.5
 

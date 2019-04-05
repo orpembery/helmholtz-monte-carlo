5e9b81fd3e8254a7d2bc2df7e609c7550ee798d4
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-04-05 14:50:55 +0100

    Test for parallel code works, although has too be run by hand, doesn't yet work within pytest.

---
+++ tests/generation_code.py
@@ -0,0 +1,26 @@
+from helmholtz_monte_carlo import error_analysis as err
+
+def qmc_test():
+    """The test used to check parallelism is working correctly."""
+    
+    k = 5.0
+
+    h_spec = (1.0,-1.5)
+
+    J = 10
+
+    nu = 20
+
+    M = 5
+
+    delta = 1.0
+
+    lambda_mult = 1.0
+
+    qois = ['integral','origin']
+
+    return err.investigate_error(k,h_spec,J,nu,M,'qmc',delta,lambda_mult,qois,num_spatial_cores=1,dim=2,display_progress=False)
+
+def serial_filename():
+    """Filename serial code is saved as."""
+    return 'serial_code_output.pickle'

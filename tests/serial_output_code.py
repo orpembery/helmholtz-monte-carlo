5712e8b01acf06ca95e78b457e1512d867468c31
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-04-15 16:29:45 +0100

    serial_output_code now uses latest syntax. Output is unchanged.

--- tests/serial_output_code.py
+++ tests/serial_output_code.py
@@ -1,7 +1,7 @@
 import generation_code as gen
 import pickle
 
-qmc_out = gen.qmc_test()
+qmc_out = gen.qmc_test(1)
 
 with open(gen.serial_filename(),'wb') as f:
             pickle.dump(qmc_out,f)

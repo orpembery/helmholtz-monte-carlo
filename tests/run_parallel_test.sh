0c9de459b9bf04084f7490d183361430ff54fd6e
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-06-11 11:04:37 +0100

    Parallel testing code now exits on first error.

--- tests/run_parallel_test.sh
+++ tests/run_parallel_test.sh
@@ -2,6 +2,10 @@
 
 # Input argument must be 1 if we are on Balena, and 0 if we are not.
 
+# Following idea nabbed from https://intoli.com/blog/exit-on-errors-in-bash-scripts/
+# Learned about exit codes from https://bencane.com/2014/09/02/understanding-exit-codes-and-how-to-use-them-in-bash-scripts/
+set -e
+
 total_procs=`cat /proc/cpuinfo | grep processor | wc -l`
 # Taken from https://www.howtogeek.com/howto/ubuntu/display-number-of-processors-on-linux/
 
